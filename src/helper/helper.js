8bf3e39488c522f18686fd94494be48f4c48a7be
Bilal Koçak <kocakbilal04@gmail.com>
2020-01-12 19:33:45 +0100

    Refactor array func

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -2,12 +2,6 @@ export function calculateDuration(time) {
     return (parseInt(time.slice(2, 5) / 60) + "h " + time.slice(2, 5) % 60 + "min");
 }
 
-export function parseArrayToString(array) {
-    let result = "";
-
-    for (let i = 0; i < array.length ; i++) {
-        result += array[i]+", "
-    }
-
-    return  result.slice(0, -2);
+export function parseArrayToString(array) { 
+    return  array.join(',')
 }
