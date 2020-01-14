7e2ac8202fcc13edbf1ff4b3a46620f0a3d566f7
Bilal Koçak <kocakbilal04@gmail.com>
2020-01-14 06:44:47 +0100

    Added animate to RatingBar

--- src/components/Common/RatingBar/RatingBar.js
+++ src/components/Common/RatingBar/RatingBar.js
@@ -4,11 +4,28 @@ import PropTypes from 'prop-types';
 import './RatingBar.sass'
 
 class RatingBar extends Component {
+    state = {
+        rateBarPercentage: 0
+    };
+
+    componentDidMount() {
+        this.calculateWidth();
+    }
+
+    calculateWidth = () => {
+        setTimeout(()=>{
+            this.setState({
+                rateBarPercentage: this.props.rate * 10 + "%"
+            })
+        },100)
+    };
+
     render() {
         const {width, rate} = this.props;
+        const {rateBarPercentage} = this.state;
         return (
             <div className={"movieRatingBar"} style={{width}}>
-                <div style={{width: (rate * 10 + "%")}}/>
+                <div style={{width: rateBarPercentage}}/>
             </div>
         );
     }
