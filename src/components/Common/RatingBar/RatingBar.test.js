49bf9ce813725d9bb7a923c9051c3a70c3ca3156
Bilal Koçak <kocakbilal04@gmail.com>
2020-01-14 03:51:29 +0100

    Created test file

---
+++ src/components/Common/RatingBar/RatingBar.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import RatingBar from "./RatingBar";
+
+Enzyme.configure({adapter: new Adapter()});
+
+describe('RatingBar', () => {
+    it("component renders correctly", () => {
+        const wrapper = shallow(<RatingBar width={'50'} rate={7.8}/>);
+        console.log(1907, wrapper.prop('style').width);
+        expect(wrapper.find(".movieRatingBar").exists()).toBe(true);
+    });
+
+    it("progress percentage correctly", () => {
+        const wrapper = shallow(<RatingBar width={'50'} rate={7.8}/>);
+        console.log(1907, wrapper.prop('style').width);
+        expect(wrapper.find(".movieRatingBar").children().prop('style').width).toBe('78%');
+    })
+});
