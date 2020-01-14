fc961acae8e9c0192fd7067c112a0fcfd269a529
Bilal Koçak <kocakbilal04@gmail.com>
2020-01-14 06:12:15 +0100

    Added control of data

--- src/components/MovieDetail/CrewInfo/CrewInfo.js
+++ src/components/MovieDetail/CrewInfo/CrewInfo.js
@@ -7,27 +7,30 @@ class CrewInfo extends Component {
     render() {
         const {writers, actors, directors} = this.props;
         return (
-            <div className={"movieCrew"}>
-                <div>
-                    <div className={"crewTitle"}>Director:</div>
-                    <div className={"crewName"}>{directors.map((item, index) => {
-                        return <span key={index}><a href="">{item}</a>{directors.length - 1 !== index && ", "}</span>
-                    })}</div>
-                </div>
-                <div>
-                    <div className={"crewTitle"}>Writers:</div>
-                    <div className={"crewName"}>{writers.map((item, index) => {
-                        return <span key={index}><a href="">{item}</a>{writers.length - 1 !== index && ", "}</span>
-                    })}</div>
-                </div>
-                <div>
-                    <div className={"crewTitle"}>Stars:</div>
-                    <div className={"crewName"}>{actors.map((item, index) => {
-                        return <span key={index}>
+            <>
+                {writers && actors && directors && <div className={"movieCrew"}>
+                    <div>
+                        <div className={"crewTitle"}>Director:</div>
+                        <div className={"crewName"}>{directors.map((item, index) => {
+                            return <span key={index}><a
+                                href="">{item}</a>{directors.length - 1 !== index && ", "}</span>
+                        })}</div>
+                    </div>
+                    <div>
+                        <div className={"crewTitle"}>Writers:</div>
+                        <div className={"crewName"}>{writers.map((item, index) => {
+                            return <span key={index}><a href="">{item}</a>{writers.length - 1 !== index && ", "}</span>
+                        })}</div>
+                    </div>
+                    <div>
+                        <div className={"crewTitle"}>Stars:</div>
+                        <div className={"crewName"}>{actors.map((item, index) => {
+                            return <span key={index}>
                             <a href="">{item}</a>{actors.length - 1 !== index && ", "} </span>
-                    })}</div>
-                </div>
-            </div>
+                        })}</div>
+                    </div>
+                </div>}
+            </>
         );
     }
 }
