14c7a394798be8ec40e5d88d640e60716cba8b4b
Bilal Koçak <kocakbilal04@gmail.com>
2020-01-14 19:00:24 +0100

    Added href and target prop to links

--- src/components/MovieDetail/CrewInfo/CrewInfo.js
+++ src/components/MovieDetail/CrewInfo/CrewInfo.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 
 import './CrewInfo.sass';
 
+const url = "https://www.google.com/search?q=";
+
 class CrewInfo extends Component {
     render() {
         const {writers, actors, directors} = this.props;
@@ -13,20 +15,24 @@ class CrewInfo extends Component {
                         <div className={"crewTitle"}>Director:</div>
                         <div className={"crewName"}>{directors.map((item, index) => {
                             return <span key={index}><a
-                                href="">{item}</a>{directors.length - 1 !== index && ", "}</span>
+                                href={url + item}
+                                target={"_blank"}>{item}</a>{directors.length - 1 !== index && ", "}</span>
                         })}</div>
                     </div>
                     <div>
                         <div className={"crewTitle"}>Writers:</div>
                         <div className={"crewName"}>{writers.map((item, index) => {
-                            return <span key={index}><a href="">{item}</a>{writers.length - 1 !== index && ", "}</span>
+                            return <span key={index}><a
+                                href={url + item}
+                                target={"_blank"}>{item}</a>{writers.length - 1 !== index && ", "}</span>
                         })}</div>
                     </div>
                     <div>
                         <div className={"crewTitle"}>Stars:</div>
                         <div className={"crewName"}>{actors.map((item, index) => {
                             return <span key={index}>
-                            <a href="">{item}</a>{actors.length - 1 !== index && ", "} </span>
+                            <a href={url + item}
+                               target={"_blank"}>{item}</a>{actors.length - 1 !== index && ", "} </span>
                         })}</div>
                     </div>
                 </div>}
