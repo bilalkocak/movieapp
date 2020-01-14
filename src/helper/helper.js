9c5223e51b722266c11c7eaf978a71935b421cdf
Bilal Koçak <kocakbilal04@gmail.com>
2020-01-14 06:12:41 +0100

    Added control of parameter

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -1,9 +1,9 @@
 export function calculateDuration(time) {
-    return (parseInt(time.slice(2, 5) / 60) + "h " + time.slice(2, 5) % 60 + "min");
+    return time && (parseInt(time.slice(2, 5) / 60) + "h " + time.slice(2, 5) % 60 + "min");
 }
 
 export function parseArrayToString(array) {
-    return array.join(',')
+    return array && array.join(',');
 }
 
 export function addFilmToWatchList(id) {
